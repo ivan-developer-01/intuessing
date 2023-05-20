cbcab3d19abf9d8dd04b9f5bb2330e79986a1c44
Ivan <127977316+ivan-developer-01@users.noreply.github.com>
2023-05-20 08:50:42 +0000

    code formatting

--- script.js
+++ script.js
@@ -1,26 +1,34 @@
-let minValue = parseInt(prompt("Минимальное знание числа для игры","0"));
-let maxValue = parseInt(prompt("Максимальное знание числа для игры","100"));
+let minValue = parseInt(prompt("Минимальное знание числа для игры", "0"));
+let maxValue = parseInt(prompt("Максимальное знание числа для игры", "100"));
+
+const btns = {
+    retry: document.querySelector("#btnRetry"),
+    over: document.querySelector("#btnOver"),
+    equal: document.querySelector("#btnEqual")
+};
+
 alert(`Загадайте любое целое число от ${minValue} до ${maxValue}, а я его угадаю`);
+
 let answerNumber  = Math.floor((minValue + maxValue) / 2);
 let orderNumber = 1;
 let gameRun = true;
 
-const orderNumberField = document.getElementById("orderNumberField");
-const answerField = document.getElementById("answerField");
+const orderNumberField = document.querySelector("#orderNumberField");
+const answerField = document.querySelector("#answerField");
 
 orderNumberField.innerText = orderNumber;
-answerField.innerText = `Вы загадали число ${answerNumber }?`;
+answerField.innerText = `Вы загадали число ${answerNumber}?`;
 
-document.getElementById("btnRetry").addEventListener("click", function () {
+btns.retry.addEventListener("click", function() {
     minValue = 0;
     maxValue = 100;
     orderNumber = 0;
-})
+});
 
-document.getElementById("btnOver").addEventListener("click", function () {
-    if (gameRun){
+btns.over.addEventListener("click", function() {
+    if (gameRun) {
         if (minValue === maxValue){
-            const phraseRandom = Math.round( Math.random());
+            const phraseRandom = Math.round(Math.random());
             const answerPhrase = (phraseRandom === 1) ?
                 `Вы загадали неправильное число!\n\u{1F914}` :
                 `Я сдаюсь..\n\u{1F92F}`;
@@ -28,18 +36,19 @@ document.getElementById("btnOver").addEventListener("click", function () {
             answerField.innerText = answerPhrase;
             gameRun = false;
         } else {
-            minValue = answerNumber  + 1;
-            answerNumber  = Math.floor((minValue + maxValue) / 2);
+            minValue = answerNumber + 1;
+            answerNumber = Math.floor((minValue + maxValue) / 2);
             orderNumber++;
+
             orderNumberField.innerText = orderNumber;
-            answerField.innerText = `Вы загадали число ${answerNumber }?`;
+            answerField.innerText = `Вы загадали число ${answerNumber}?`;
         }
     }
-})
+});
 
-document.getElementById("btnEqual").addEventListener("click", function () {
-    if (gameRun){
-        answerField.innerText = `Я всегда угадываю\n\u{1F60E}`
+btns.equal.addEventListener("click", function () {
+    if (gameRun) {
+        answerField.innerText = `Я всегда угадываю\n\u{1F60E}`;
         gameRun = false;
     }
 })
